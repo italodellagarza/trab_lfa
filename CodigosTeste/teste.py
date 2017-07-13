61323380fb2e681bb0604cfef4665bc9eea34d03
brunoqs <brun_o13@hotmail.com>
2017-07-13 13:51:21 -0300

    aninhando classes em py

--- CodigosTeste/teste.py
+++ CodigosTeste/teste.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3.5
 
-class teste:
+class teste1:
 	def __init__(self):
-		self.arquivo = open("desc_af1.txt", 'r')
+		self.arquivo = open("../desc_af1.txt", 'r')
 		self.arquivo.readline() # lendo so a primeira linha
 		self.estados = self.arquivo.readline().split()
 
+class teste:
+	def __init__(self):
+		self.t = teste1()
+
 # main
 if __name__ == "__main__":
-    t = teste()
-    print (t.estados[2])
+    teste = teste()
+    print (teste.t.estados)
