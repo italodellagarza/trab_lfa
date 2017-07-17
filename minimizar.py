51b53d7c994db860bc64c44685f4c8fc7f1cb1d9
Italo Della Garza Silva <italodellagarza@hotmail.com>
2017-07-17 13:21:30 -0300

    falta arrumar Estado inicial

--- minimizar.py
+++ minimizar.py
@@ -22,11 +22,12 @@ class Transicao(object):
         # A transição vai de estadoAtual para estadoSeguinte
         self.estadoAtual = estadoAtual
         self.estadoSeguinte = estadoSeguinte
+        self.letras = []
         self.letras.append(letras)              # Lista de letras do alfabeto
                                                 # em que é feita a transição
 
 class Automato(object):
-    def __init__(self):
+    def __init__(self, arquivoEntrada):
         '''Construtor de classe.'''
         entrada = open(arquivoEntrada, 'r')
         # Aqui, começamos a pegar todos os estados do arquivo e eliminar caracteres inúteis:
@@ -43,29 +44,43 @@ class Automato(object):
             self.alfabeto.append(letra)
 
         # Para as transições, temos que ler várias linhas. Precisamos de um critério de parada:
-        entrada.readline()
+        entrada.readline() # Lê o '{'
         # Acusa como errado. Testar:
-        while temp = entrada.readline().replace(",", " ").replace("(", "").replace("),","").replace("->","").split():
+        temp = entrada.readline()
+        self.transicoes = []
+        while temp[0] == '(':
+            temp = temp.replace(",", " ").replace("(", "").replace("),", "").replace("->", " ").split()
             ja_existe = False
-            for estado in self.estados:
-                if estado.estadoAtual == temp[0] and estado.estadoSeguinte == temp[2]:
+            for transicao in self.transicoes:
+                if transicao.estadoAtual.nomeEstado == temp[0] and transicao.estadoSeguinte.nomeEstado == temp[2]:
                     ja_existe = True
-                    estado.letras.append(temp[1])
+                    transicao.letras.append(temp[1])
             if not ja_existe:
-                self.transicoes.append(Transicao(temp[0],temp[2],temp[1]))
+                ida = Estado(temp[0])
+                chega = Estado(temp[2])
+                for estado in self.estados:
+                    if estado.nomeEstado == ida.nomeEstado:
+                        ida = estado
+                    if estado.nomeEstado == chega.nomeEstado:
+                        chega = estado
+                self.transicoes.append(Transicao(ida, chega, temp[1]))
+            temp = entrada.readline()
 
         # Para o estado inicial, basta ler seu nome e buscá-lo na lista já existente:
-        entrada.readline()
         temp = entrada.readline().replace(",", "")
+        print("inicial = " + temp)
+        self.inicial = self.estados[0]
         for estado in self.estados:
+            print("aqui1")
             if estado.nomeEstado == temp:
+                print("aqui1")
                 self.inicial = estado
 
         # Para os estados finais, procurar cada um na lista de estados e setá-los como final = True
         temp = entrada.readline().replace(",", " ").replace("{", "").replace("}", "").split()
         for tempEstado in temp:
             for estado in self.estados:
-                if tempEstado.nomeEstado == estado.nomeEstado:
+                if tempEstado == estado.nomeEstado:
                     estado.final = True
 
         # TODO TESTAR E CORRIGIR!!!!
@@ -81,7 +96,18 @@ class Automato(object):
 
 # main
 if __name__ == "__main__":
-    #print ("TODO")
-    #e = Estado()
-    #print (e.nomeEstado)
-    #print (len(e.final))
+    print ("TODO")
+    a = Automato("desc_af1.txt")
+    for estado in a.estados:
+        print(estado.nomeEstado)
+        if estado.final:
+            print('final')
+    for transicao in a.transicoes:
+        print(transicao.estadoAtual.nomeEstado + " -> " + transicao.estadoSeguinte.nomeEstado)
+        print(transicao.letras)
+
+    print (a.estados[0] == a.transicoes[0].estadoAtual) # está apontando corretamente
+
+    print (a.inicial.nomeEstado)
+
+
