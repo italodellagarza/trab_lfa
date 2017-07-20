f7d017b61142ca5c95b4688751abd453925015f3
brunoqs <brun_o13@hotmail.com>
2017-07-20 17:49:08 -0300

    loop infinito no while

--- minimizar.py
+++ minimizar.py
@@ -10,6 +10,9 @@
    O autômato é aqui formalizado em uma quíntupla (estados, alfabeto, transições, iniciais e finais)
    Na qual a informação de estado final é armazenada na própria estrutura Estado'''
 
+# Biblioteca necessaria para utilizar argv 
+import sys; 
+
 class Estado(object):
     def __init__(self, nomeEstado):
         '''Construtor de classe'''
@@ -17,19 +20,17 @@ class Estado(object):
         self.final = False
 
 class Transicao(object):
-    def __init__(self, estadoAtual, estadoSeguinte, letras):
+    def __init__(self, estadoAtual, estadoSeguinte, letra):
         '''Construtor de classe'''
         # A transição vai de estadoAtual para estadoSeguinte
         self.estadoAtual = estadoAtual
         self.estadoSeguinte = estadoSeguinte
-        self.letras = []
-        self.letras.append(letras)              # Lista de letras do alfabeto
-                                                # em que é feita a transição
+        self.letra = letra
 
 class Automato(object):
-    def __init__(self, arquivoEntrada):
+    def __init__(self):
         '''Construtor de classe.'''
-        entrada = open(arquivoEntrada, 'r')
+        entrada = open(sys.argv[1], 'r')
         # Aqui, começamos a pegar todos os estados do arquivo e eliminar caracteres inúteis:
         entrada.readline()
         temp = entrada.readline().replace(",", " ").replace("{", "").replace("}","").split()
@@ -50,20 +51,14 @@ class Automato(object):
         self.transicoes = []
         while temp[0] == '(':
             temp = temp.replace(",", " ").replace("(", "").replace(")", "").replace("->", " ").split()
-            ja_existe = False
-            for transicao in self.transicoes:
-                if transicao.estadoAtual.nomeEstado == temp[0] and transicao.estadoSeguinte.nomeEstado == temp[2]:
-                    ja_existe = True
-                    transicao.letras.append(temp[1])
-            if not ja_existe:
-                ida = Estado(temp[0])
-                chega = Estado(temp[2])
-                for estado in self.estados:
-                    if estado.nomeEstado == ida.nomeEstado:
-                        ida = estado
-                    if estado.nomeEstado == chega.nomeEstado:
-                        chega = estado
-                self.transicoes.append(Transicao(ida, chega, temp[1]))
+            ida = Estado(temp[0])
+            chega = Estado(temp[2])
+            for estado in self.estados:
+                if estado.nomeEstado == ida.nomeEstado:
+                    ida = estado
+                if estado.nomeEstado == chega.nomeEstado:
+                    chega = estado
+            self.transicoes.append(Transicao(ida, chega, temp[1]))
             temp = entrada.readline()
 
         # Para o estado inicial, basta ler seu nome e buscá-lo na lista já existente:
@@ -84,32 +79,75 @@ class Automato(object):
 
         entrada.close()
 
-    def minimizaAutomato(self, automatoSaida, tabelaSaida):
-        automato = open(automatoSaida)
-        tabela = open(tabelaSaida)
+    def minimizaAutomato(self):
+        #automato = open(sys.argv[2], 'w')
+        #tabela = open(sys.argv[3], 'w')
         # TODO (Tratar por estrutura, se necessário)
         # TODO 1. O programa cria uma lista de pares (para cada par, define-se  um boolean D[i,j], uma lista de pares S[i,j] e uma string Motivo)
         # TODO 2. D[final,nao-final] e D[nao-final,final] = False
-        # TODO 3. Para cada par, compara-se suas transições.
-        # TODO 3.1 Se transição de par[1] -> final e transição par[2]-> nao final ou o contrário, D[i,j] = false para esse par e todos os seus S[i,j]
+        lista_pares = []
+        for e1 in range(len(self.estados)):
+        	for e2 in range(len(self.estados)-e1-1):
+        		lista_pares.append(Par(self.estados[e1], self.estados[e1 + e2 + 1]))
+
+        for par in lista_pares:
+        	if par.estado1.final != par.estado2.final:
+        		par.dij = False
+        		par.motivo = "final/nao final"
+        for par in lista_pares:
+        	lista_t1 = self.transicoesDeEstado(par.estado1)
+        	lista_t2 = self.transicoesDeEstado(par.estado2)
+        	for t1 in lista_t1:
+        		for t2 in lista_t2:
+        			if t1.letra == t2.letra:
+        				for par_seguinte in lista_pares:
+        					while not ((t1.estadoSeguinte == par_seguinte.estado1 and t2.estadoSeguinte == par_seguinte.estado2) or
+        					          (t2.estadoSeguinte == par_seguinte.estado1 and t1.estadoSeguinte == par_seguinte.estado2)):
+        						pass
+        					if not par_seguinte.dij:
+        						par.dij = False
+        						par.motivo = t1.letra # ou t2.letra
+        						for dependencias in par.sij:
+        							dependencias.dij = False
+        							dependencias.motivo = ("prop[", par.estado1.nomeEstado, ",", par.estado2.nomeEstado,"]")
+        					else:
+        						par_seguinte.sij.append(par)
         # TODO 3.2 Senao:
-        tabela.close()
-        automato.close()
+		#tabela.close()
+		#automato.close()
+
+    def transicoesDeEstado(self, estado):
+    	listaTransicoes = []
+    	for transicao in self.transicoes:
+    		if transicao.estadoAtual == estado:
+    			listaTransicoes.append(transicao)
+    	return listaTransicoes
+
+class Par:
+	def __init__(self, estado1, estado2):
+		self.estado1 = estado1
+		self.estado2 = estado2
+		self.dij = True
+		self.sij = []
+		self.motivo = ""
+	
+
+
 
 # Função Principal
 if __name__ == "__main__":
-    print ("TODO")
-    a = Automato("desc_af1.txt")
-    for estado in a.estados:
-        print(estado.nomeEstado)
-        if estado.final:
-            print('final')
-    for transicao in a.transicoes:
-        print(transicao.estadoAtual.nomeEstado + " -> " + transicao.estadoSeguinte.nomeEstado)
-        print(transicao.letras)
+	print ("TODO")
+	a = Automato()
+	a.minimizaAutomato()
+'''	for estado in a.estados:
+		print(estado.nomeEstado)
+		if estado.final:
+			print('final')
+	for transicao in a.transicoes:
+		print(transicao.estadoAtual.nomeEstado + " -> " + transicao.estadoSeguinte.nomeEstado)
+		print(transicao.letras)
 
-    print(a.estados[0] == a.transicoes[0].estadoAtual) # está apontando corretamente
+	print(a.estados[0] == a.transicoes[0].estadoAtual) # está apontando corretamente
 
-    print(a.inicial.nomeEstado)
-
+	print(a.inicial.nomeEstado)'''
 
